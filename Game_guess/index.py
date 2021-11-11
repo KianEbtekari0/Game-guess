1e746770a03010043aeae8e6bdc982414751d63d
Kian Ebtekari <92510927+KianEbtekari0@users.noreply.github.com>
2021-11-11 13:09:54 +0330

    Add files via upload

---
+++ Game_guess/index.py
@@ -0,0 +1,43 @@
+print('    *')
+print('   ---')
+print('  ------ ')
+print(' --------')
+
+print('  --  -- ')
+print('   ----  ')
+print('What is my name?')
+print('guess')
+
+count = 0
+limit = 3
+name = "kian"
+k = "k"
+kia = "kia"
+
+while count < limit:
+    start = str(input('Guess: '))
+    count += 1
+    if start == name:
+        print('you won!')
+        break
+    
+    if start == k:
+        print('one k')
+
+    if start == kia:
+        print('mmm.ok')
+    
+    else:
+        print('No...')
+
+else:
+    print('sorry, you faild')
+
+if count == 1:
+    print('you very good')
+
+if count == 2:
+    print('it was not bad. but you won')
+
+if count == 3:
+    print('It was bad, but you won')
